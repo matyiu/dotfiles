778e064a738b1cc3c5bf0e03d5905a581c82d3b3
Jefferson Valle <valleespinozajefferson@gmail.com>
2023-07-30 19:33:28 +0200

    Fixes again

--- sync/backup_config.js
+++ sync/backup_config.js
@@ -2,13 +2,13 @@ const { mkdirSync, renameSync } = require('fs')
 const { BACKUP_DIR, SCRIPTS_DIR, CONFIG_DIR, DIRECTORIES, DOTFILES_DIR} = require("./constants");
 
 const backup_config = () => {
-    mkdirSync(BACKUP_DIR)
-    mkdirSync(BACKUP_DIR + '/.scripts')
-    mkdirSync(BACKUP_DIR + '/.config')
-    mkdirSync(SCRIPTS_DIR)
+    mkdirSync(BACKUP_DIR, { recursive: true })
+    mkdirSync(BACKUP_DIR + '/.scripts', { recursive: true })
+    mkdirSync(BACKUP_DIR + '/.config', { recursive: true })
+    mkdirSync(SCRIPTS_DIR, { recursive: true })
 
     DIRECTORIES.forEach((dir) => {
-        renameSync(DOTFILES_DIR + `/${dir}`, `${BACKUP_DIR}/.scripts/${dir}`)
+        renameSync(DOTFILES_DIR + `/${dir}`, `${BACKUP_DIR}/${dir}`)
     })
 
     renameSync(SCRIPTS_DIR, BACKUP_DIR + '/.scripts')
