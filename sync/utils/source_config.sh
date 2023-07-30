b60be1f66327e329ef1740beb1263a74b5ca4b8b
Jefferson Valle <valleespinozajefferson@gmail.com>
2023-07-30 19:08:25 +0200

    Fixes

--- sync/utils/source_config.sh
+++ sync/utils/source_config.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 ZSH_FILE=$HOME/.zshrc
-BASH_FILE=$HOME/.bashrc
 
 source_config() {
   if [[ -f "$ZSH_FILE" ]]; then
-    source $ZSH_FILE
+    source "$HOME"/.zshrc
   else
-    source $BASH_FILE
+    source "$HOME"/.bashrc
   fi
 }
 
