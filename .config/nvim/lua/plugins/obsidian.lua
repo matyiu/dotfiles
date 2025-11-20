dfee1e40e07cb89975c4fba5f7c42192528910e3
Jefferson Valle <valleespinozajefferson@gmail.com>
2025-11-20 12:20:17 +0300

    feat(nvim): replace legacy obsidian plugin

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -1,8 +1,8 @@
 return {
   {
-    "epwalsh/obsidian.nvim",
-    version = "3.7",
-    lazy = true,
+    "obsidian-nvim/obsidian.nvim",
+    version = "*",
+    lazy = false,
     ft = "markdown",
     -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
     -- event = {
@@ -15,10 +15,23 @@ return {
       "nvim-lua/plenary.nvim",
     },
     opts = {
+      legacy_commands = false,
+      -- obsidian_command = "setsid /usr/bin/obsidian",
       workspaces = {
         {
           name = "personal",
           path = "~/Nextcloud/Personal/Notes",
+          overrides = {
+            -- new_notes_location = "0 - Index",
+            note_id_func = function(title)
+              local timestamp = os.date "%Y%m%d%H%M"
+              local suffix = ""
+              if title ~= nil and title ~= "" then
+                suffix = "-" .. title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+              end
+              return timestamp .. suffix
+            end
+          },
         }
       },
     },
