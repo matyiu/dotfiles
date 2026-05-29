e8407520ba7303bfd28e59a439c3583e0ee27742
Jefferson Valle <valleespinozajefferson@gmail.com>
2026-05-29 11:29:46 +0300

    feat(alias): removing nt & npm for pnpm

--- .oh-my-zsh/custom/dev.zsh
+++ .oh-my-zsh/custom/dev.zsh
@@ -7,6 +7,7 @@ alias ndev="npm run dev"
 alias ntsc="npm run tsc"
 alias nlint="npm run lint"
 alias kb="kubectl"
+alias npm="pnpm"
 
 alias vim="nvim"
 alias kadeck='docker run -p 80:80 -e xeotek_kadeck_free="valleespinozajefferson@gmail.com" -e xeotek_kadeck_port=80 xeotek/kadeck:5.1.11'
